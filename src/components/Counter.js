4a0f925deda07e8078e2bf7491631edbab9f28a8
Zidane Arkan <muhammad.zidane@sera.astra.co.id>
2024-10-27 17:19:05 +0700

    Connect redux to components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,23 @@
-import classes from './Counter.module.css';
-
+import { useState } from "react";
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
-  const toggleCounterHandler = () => {};
-
+  const toggleCounterHandler = () => {
+    setToogleCounter((state) => !state);
+  };
+  const count = useSelector((state) => state.count);
+  const [tooggleCounter, setToogleCounter] = useState(false);
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{count}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      {tooggleCounter && (
+        <div className={classes.counterBox}>
+          <button>+</button>
+          <button>-</button>
+        </div>
+      )}
     </main>
   );
 };
