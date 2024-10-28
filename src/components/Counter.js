9a4ba7dcbbfc8addc4b95e6d4c4cdb3fb883bf31
Zidane Arkan <muhammad.zidane@sera.astra.co.id>
2024-10-29 06:14:21 +0700

    Add another state [Show/Hide state]

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,26 @@
-import { useState } from "react";
+// import { useState } from "react";
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {
-    setToogleCounter((state) => !state);
-  };
+  const count = useSelector((state) => state.count);
+  const show = useSelector((state) => state.showCounter);
+  console.log(show);
+  // const toggleCounterHandler = () => {
+  //   setToogleCounter((state) => !state);
+  // };
 
   const incrementHandler = () => dispatch({ type: "increment" });
   const increaseByTen = () => dispatch({ type: "increase", value: 10 });
   const decrementHandler = () => dispatch({ type: "decrement" });
 
-  const count = useSelector((state) => state.count);
-  const [tooggleCounter, setToogleCounter] = useState(false);
+  const toggleCounterHandler = () => dispatch({ type: "toogle" });
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{count}</div>
-      {tooggleCounter && (
+      {show && (
         <div className={classes.counterBox}>
           <button onClick={incrementHandler}>+</button>
           <button onClick={increaseByTen}>+ 10</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,36 @@
 // import redux from "react-redux";
 import { legacy_createStore as createStore } from "redux";
 
-const counterReducer = (state = { count: 0 }, action) => {
+const initialState = { count: 0, showCounter: false };
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       count: state.count + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increase") {
     return {
       count: state.count + action.value,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       count: state.count - 1,
+      showCounter: state.showCounter,
     };
   }
+
+  if (action.type === "toogle") {
+    return {
+      showCounter: !state.showCounter,
+      count: state.count,
+    };
+  }
+
   return state;
 };
 
