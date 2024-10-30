c07b257ef40062f0eaa00b9a4a830fa4b2e823aa
Zidane Arkan <muhammad.zidane@sera.astra.co.id>
2024-10-30 08:20:11 +0700

    Using object spread to copy obj

--- src/store/index.js
+++ src/store/index.js
@@ -5,29 +5,33 @@ const initialState = { count: 0, showCounter: false };
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
+      ...state,
       count: state.count + 1,
-      showCounter: state.showCounter,
+      // showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increase") {
     return {
+      ...state,
       count: state.count + action.value,
-      showCounter: state.showCounter,
+      // showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
+      ...state,
       count: state.count - 1,
-      showCounter: state.showCounter,
+      // showCounter: state.showCounter,
     };
   }
 
   if (action.type === "toogle") {
     return {
+      ...state,
       showCounter: !state.showCounter,
-      count: state.count,
+      // count: state.count,
     };
   }
 
